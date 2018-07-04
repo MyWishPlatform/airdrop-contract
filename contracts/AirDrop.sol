8f13537613fa87334fc06ae128137a986ad45471
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-07-04 18:29:47 +0300

    support erc223

--- contracts/AirDrop.sol
+++ contracts/AirDrop.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "sc-library/contracts/ERC223/ERC223Receiver.sol";
+
 
 contract AirDrop is Ownable {
     ERC20 public token;
@@ -10,14 +12,6 @@ contract AirDrop is Ownable {
         owner = _target;
         token = _token;
         createdAt = block.number;
-    }
-
-    function withdraw(uint _value) public onlyOwner {
-        token.transfer(owner, _value);
-    }
-
-    function withdrawToken(address _token, uint _value) public onlyOwner {
-        ERC20(_token).transfer(owner, _value);
     }
 
     function transfer(address[] _addresses, uint[] _amounts) external onlyOwner {
@@ -30,9 +24,21 @@ contract AirDrop is Ownable {
 
     function transferFrom(address _from, address[] _addresses, uint[] _amounts) external onlyOwner {
         require(_addresses.length == _amounts.length);
-        
+
         for (uint i = 0; i < _addresses.length; i ++) {
             token.transferFrom(_from, _addresses[i], _amounts[i]);
         }
+    }
+
+    function tokenFallback(address _from, uint _value, bytes _data) public {
+        // receive tokens
+    }
+
+    function withdraw(uint _value) public onlyOwner {
+        token.transfer(owner, _value);
+    }
+
+    function withdrawToken(address _token, uint _value) public onlyOwner {
+        ERC20(_token).transfer(owner, _value);
     }
 }
